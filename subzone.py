24bdcb47b865a665194ebc2752893c6a535c31e6
gume <gume75@gmail.com>
2023-01-31 00:05:56 +0100

    Add some logic (TRV control)

--- subzone.py
+++ subzone.py
@@ -13,12 +13,12 @@ from homeassistant.components.sensor import SensorEntity, SensorDeviceClass, Uni
 from homeassistant.components.binary_sensor import BinarySensorEntity
 from homeassistant.helpers.event import async_track_state_change_event
 from homeassistant.helpers.entity import Entity
-
+from homeassistant.exceptions import ServiceNotFound
 
 from .const import (
     MANUFACTURER, VERSION, NAME,
     DOMAIN,
-    CONF_ZONES, CONF_PUMPS, CONF_SUBZONES, CONF_SENSOR,
+    CONF_ZONES, CONF_PUMPS, CONF_SUBZONES, CONF_SENSOR, CONF_VALVES, CONF_SWITCH,
     CONF_MAIN, CONF_MODES,
     remove_platform_name,
     SERVICE_SUBZONE_PRESET_MODE,
@@ -49,9 +49,33 @@ class SubZone(SwitchEntity):
         self._mode = SubZoneMode(self, self._device_name)
         self._entities += [ self._mode ]
         
-        self.hass.states.async_set(f"subzone.{self.name}" , self._mode.state)
+        self._valves = []
+        self._valve_states = {}
+        if CONF_VALVES in config:
+            for cv  in config[CONF_VALVES]:
+                switch = cv[CONF_SWITCH]
+                self._valves.append(switch)
+                self._valve_states[switch] = "unknown"
+
+        """ Listen on valve changes and force them to follow the subzone requirements """
+        if len(self._valves) > 0:
+            _LOGGER.debug(self._valves)
+            self._listen_valves = async_track_state_change_event(self.hass, self._valves, self.async_track_valve_state_change_event)
+            self.hass.async_create_task(self.async_control_valves())
+                
+        self.hass.async_create_task(self.async_update())
+
+    async def async_update(self):
+        """ Update the subzone.xxx state with the actual states """
+        attributes = {}
+        attributes["temperature"] = self._temp.state
+        attributes["heating"] = self._state
+        attributes["valves"] = len(self._valves)
+
+        self.hass.states.async_set(f"subzone.{self.name}" , self._mode.state, attributes)
 
     async def async_call(self, service: str, call: ServiceCall):
+        """ Service call dispatcher/ Returns false is there was no processing """
         if  f"subzone.{self.name}" in call.data[CONF_ENTITY_ID]:
             if service == SERVICE_SUBZONE_PRESET_MODE:
                 return await self.async_call_subzone_preset_mode(call)
@@ -59,21 +83,46 @@ class SubZone(SwitchEntity):
         return False
 
     async def async_call_subzone_preset_mode(self, call: ServiceCall):
+        """ Set the subzone mode """
         new_mode = call.data["preset_mode"]
         result = await self._mode.async_set_preset_mode(new_mode)
         if result:
-            self.hass.states.async_set(f"subzone.{self.name}" , self._mode.state)
+            self.async_schedule_update_ha_state(True)
         return result
 
+    async def async_control_valves(self):
+        """ Set valves according to the subzone state """
+        """ Valve states are cached, use them """
+        for v, s in self._valve_states.items():
+            if s != self._state:
+                try:
+                    await self.hass.services.async_call("switch", f"turn_{self._state}", {"entity_id": v})
+                except ServiceNotFound as e:
+                    _LOGGER.warning(str(e))
+
+    async def async_track_valve_state_change_event(self, event):
+        if event.data[CONF_ENTITY_ID] in self._valves:
+            v, s = (event.data[CONF_ENTITY_ID], event.data.get("new_state").state)
+            self._valve_states[v] = s
+            if s != self._state:
+                """ Schedule a control """
+                self.hass.async_create_task(self.async_control_valves())
+        """ Scedule update """
+        self.async_schedule_update_ha_state()
+
     async def async_turn_on(self, **kwargs):  # pylint: disable=unused-argument
+        """ This is the interface to the climate entity. The climate entity will switch it """
         self._state = "on"
         self._attr_icon = "mdi:radiator"
         self.async_write_ha_state()
+        self.hass.async_create_task(self.async_control_valves())
 
     async def async_turn_off(self, **kwargs):  # pylint: disable=unused-argument
+        """ This is the interface to the climate entity. The climate entity will switch it """
         self._state = "off"
         self._attr_icon = "mdi:radiator-off"
         self.async_write_ha_state()
+        self.hass.async_create_task(self.async_control_valves())
 
     @property
     def state(self):
@@ -121,6 +170,7 @@ class SubZoneTemperature(SensorEntity):
         self._temperature = event.data.get("new_state").state
         self._attr_available = True
         self.async_write_ha_state()
+        self._zone.async_schedule_update_ha_state(True)
 
     @property
     def state(self):
@@ -162,6 +212,7 @@ class SubZoneMode(SensorEntity):
         if preset in CONF_MODES:
             self._state = preset
             self.async_write_ha_state()
+            return True
         else:
             return False
 
